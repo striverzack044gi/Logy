44c597c430c3c17d7fbe8c3a45edec02f639c54c
striverzack044gi <striverzack044@gmail.com>
2026-09-04 21:25:15 +0530

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,1 +1,12 @@
+from api.api import create_app
 
+
+def test_api():
+
+    app = create_app()
+
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
