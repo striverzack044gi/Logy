85cf10882d1b00be0356adc8e53611315bb37c10
striverzack044gi <striverzack044@gmail.com>
2026-09-04 21:24:14 +0530

    Update test_memory.py

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,1 +1,18 @@
+from memory.memory import Memory
 
+
+def test_memory(tmp_path):
+
+    memory_file = tmp_path / "memory.json"
+
+    memory = Memory(memory_file)
+
+    memory.add(
+        "Hello",
+        "Hi Sir!"
+    )
+
+    data = memory.load()
+
+    assert len(data) == 1
+    assert data[0]["user"] == "Hello"
