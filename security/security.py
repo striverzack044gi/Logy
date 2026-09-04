294c9a4f8a3e0664ec0a3bc503402839dc8c01ee
striverzack044gi <striverzack044@gmail.com>
2026-09-04 21:22:40 +0530

    Update security.py

--- security/security.py
+++ security/security.py
@@ -1,1 +1,19 @@
+import hashlib
 
+
+class Security:
+
+    @staticmethod
+    def hash_text(text: str):
+
+        return hashlib.sha256(
+            text.encode("utf-8")
+        ).hexdigest()
+
+    @staticmethod
+    def verify_text(text: str, hashed: str):
+
+        return (
+            Security.hash_text(text)
+            == hashed
+        )
