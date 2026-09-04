2c1448ad5889e8fd618d0a55b5a64145c940c3fc
striverzack044gi <striverzack044@gmail.com>
2026-09-04 21:20:11 +0530

    Update voice_config.py

--- voice/voice_config.py
+++ voice/voice_config.py
@@ -1,1 +1,15 @@
+VOICE_CONFIG = {
 
+    "language": "bn",
+
+    "supported_languages": [
+        "bn",
+        "en"
+    ],
+
+    "voice_style": "cute_soft_female",
+
+    "speed": 1.0,
+
+    "pitch": 1.0
+}
