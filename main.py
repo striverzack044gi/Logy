56e2721db41ff4d6f1268435383774b2ed9a1a7f
striverzack044gi <striverzack044@gmail.com>
2026-09-04 21:25:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from api.api import create_app
+from config.config import HOST, PORT, DEBUG
 
+
+app = create_app()
+
+
+if __name__ == "__main__":
+
+    print("================================")
+    print("          LOGY AI")
+    print("================================")
+    print(f"Server: http://{HOST}:{PORT}")
+    print("Status: Online")
+    print("================================")
+
+    app.run(
+        host=HOST,
+        port=PORT,
+        debug=DEBUG
+    )
