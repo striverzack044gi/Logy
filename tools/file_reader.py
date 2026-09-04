6b492c3e2d03979283dc5061cfa92c51a9bbe31a
striverzack044gi <striverzack044@gmail.com>
2026-09-04 21:17:33 +0530

    Update file_reader.py

--- tools/file_reader.py
+++ tools/file_reader.py
@@ -1,1 +1,25 @@
+from pathlib import Path
 
+
+def read_text_file(file_path):
+
+    path = Path(file_path)
+
+    if not path.exists():
+        return None
+
+    if not path.is_file():
+        return None
+
+    try:
+
+        return path.read_text(
+            encoding="utf-8"
+        )
+
+    except UnicodeDecodeError:
+
+        return path.read_text(
+            encoding="utf-8",
+            errors="ignore"
+        )
